a1657b043b191a05c961e9d242ff06b5597b01c1
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-01-24 14:21:48 +0000

    Added router handlers to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HomePage } from "./routes/HomePage";
+import { IndividualFriend } from "./routes/IndividualFriend";
+import { IndividualExpense } from "./routes/IndividualExpense";
+import { AddExpense } from "./routes/AddExpense";
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/friends/:id" element={<IndividualFriend />} />
+          <Route path="/expenses/:id" element={<IndividualExpense />} />
+          <Route path="/expenses/add" element={<AddExpense />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
