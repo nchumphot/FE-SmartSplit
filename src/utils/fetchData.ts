a455ec97581be17afc58fb47b3e19b497b5a6e4f
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-01-25 17:17:38 +0000

    Added typing for the return type of fetchData

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,11 +1,13 @@
 import { IUser } from "../interfaces/IUser";
 import { ISummary } from "../interfaces/ISummary";
+import { IFriend } from "../interfaces/IFriend";
 
 export function fetchData(
   url: string,
   setState:
     | React.Dispatch<React.SetStateAction<IUser[]>>
     | React.Dispatch<React.SetStateAction<ISummary | undefined>>
+    | React.Dispatch<React.SetStateAction<IFriend | undefined>>
 ): void {
   fetch(url)
     .then((res) => res.json())
