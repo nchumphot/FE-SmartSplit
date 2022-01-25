11f49a15e2961de74736b01de554ac89a64b2805
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-01-25 10:57:59 +0000

    Added ISummary to the typing

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,8 +1,11 @@
 import { IUser } from "../interfaces/IUser";
+import { ISummary } from "../interfaces/ISummary";
 
 export function fetchData(
   url: string,
-  setState: React.Dispatch<React.SetStateAction<IUser[]>>
+  setState:
+    | React.Dispatch<React.SetStateAction<IUser[]>>
+    | React.Dispatch<React.SetStateAction<ISummary | undefined>>
 ): void {
   fetch(url)
     .then((res) => res.json())
