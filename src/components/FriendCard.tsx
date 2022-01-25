09943db52d1df498fe8b40d8b950a63aeb56dd18
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-01-25 15:36:22 +0000

    Friend card now returns the relevant information

--- src/components/FriendCard.tsx
+++ src/components/FriendCard.tsx
@@ -1,6 +1,14 @@
 import { IFriendSummary } from "../interfaces/IFriendSummary";
+import { ISummary } from "../interfaces/ISummary";
+import { IUser } from "../interfaces/IUser";
+import { handleAddStranger } from "../utils/handleAddStranger";
 
-export function FriendCard(props: { friend: IFriendSummary }): JSX.Element {
+export function FriendCard(props: {
+  friend: IFriendSummary;
+  isFriend: boolean;
+  user: IUser;
+  setSummary: React.Dispatch<React.SetStateAction<ISummary | undefined>>;
+}): JSX.Element {
   if (props.friend.balance === undefined) {
     return (
       <>
@@ -9,16 +17,49 @@ export function FriendCard(props: { friend: IFriendSummary }): JSX.Element {
     );
   } else {
     return (
-      <>
-        <h4>{props.friend.name}</h4>
-        {props.friend.balance < 0 && (
-          <p>You are owed £{(props.friend.balance * -1).toFixed(2)}</p>
-        )}
-        {props.friend.balance > 0 && (
-          <p>You owe £{props.friend.balance.toFixed(2)}</p>
-        )}
-        {props.friend.balance === 0 && <p>You are settled up</p>}
-      </>
+      <div
+        className={`container m-2 p-2 border ${
+          props.isFriend ? "border border-success" : "border border-warning"
+        }`}
+      >
+        <div className="row">
+          <div className="col-10">
+            <h4>{props.friend.name}</h4>
+            {props.isFriend === false && (
+              <button
+                className="btn btn-success"
+                onClick={() =>
+                  handleAddStranger(
+                    props.user.id,
+                    props.friend.id,
+                    props.setSummary
+                  )
+                }
+              >
+                Add friend
+              </button>
+            )}
+          </div>
+          <div className="col">
+            {props.friend.balance < 0 && (
+              <p>
+                You are owed <br />£{(props.friend.balance * -1).toFixed(2)}
+              </p>
+            )}
+            {props.friend.balance > 0 && (
+              <p>
+                You owe <br />£{props.friend.balance.toFixed(2)}
+              </p>
+            )}
+            {props.friend.balance === 0 && (
+              <p>
+                You are <br />
+                settled up
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
     );
   }
 }
