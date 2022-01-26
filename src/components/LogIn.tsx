1498639faa0c91dc8891348c64a5549b4245e258
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-01-26 15:38:48 +0000

    Added loading users message

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -7,30 +7,41 @@ export default function LogIn(props: {
   setAllUsers: React.Dispatch<React.SetStateAction<IUser[]>>;
   setUser: React.Dispatch<React.SetStateAction<IUser | undefined>>;
 }): JSX.Element {
-  return (
-    <div>
-      <label htmlFor="login">Log in:</label>
-      <select
-        name="login"
-        defaultValue={""}
-        onChange={(e) => {
-          props.setUser(getUserById(props.allUsers, parseInt(e.target.value)));
-        }}
-      >
-        <option value="" disabled>
-          Please select your name to log in
-        </option>
-        {props.allUsers.map((user) => (
-          <option key={user.id} value={user.id}>
-            {user.name}
+  if (props.allUsers.length === 0) {
+    return (
+      <div>
+        <label htmlFor="login">Log in:</label>
+        <p>Loading users...</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <label htmlFor="login">Log in:</label>
+        <select
+          name="login"
+          defaultValue={""}
+          onChange={(e) => {
+            props.setUser(
+              getUserById(props.allUsers, parseInt(e.target.value))
+            );
+          }}
+        >
+          <option value="" disabled>
+            Please select your name to log in
           </option>
-        ))}
-      </select>
-      <Link to="/signup">
-        <button type="button" className="btn btn-primary">
-          Sign up
-        </button>
-      </Link>
-    </div>
-  );
+          {props.allUsers.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+        <Link to="/signup">
+          <button type="button" className="btn btn-primary">
+            Sign up
+          </button>
+        </Link>
+      </div>
+    );
+  }
 }
