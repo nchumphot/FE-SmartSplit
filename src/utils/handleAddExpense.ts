cf0096dda51625c949efa3fb54840d39809e0648
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-02-07 22:02:04 +0700

    Amount must pass a RegEx test before an expense can be added

--- src/utils/handleAddExpense.ts
+++ src/utils/handleAddExpense.ts
@@ -2,18 +2,23 @@ import axios from "axios";
 import { IExpenseForm } from "../interfaces/IExpenseForm";
 import { ITransactionAdd } from "../interfaces/ITransactionAdd";
 import { baseUrl } from "./baseUrl";
+import { verifyAmount } from "./verifyAmount";
 
 export async function handleAddExpense(
   userId: number | undefined,
   details: IExpenseForm,
   transactions: ITransactionAdd[]
 ): Promise<void> {
-  axios.post(baseUrl + `/expenses`, {
-    userId: userId,
-    description: details.description,
-    transactionDate: details.date,
-    totalBalance: details.amount,
-    notes: details.notes,
-    transactions: transactions,
-  });
+  if (verifyAmount(details.amount)) {
+    axios.post(baseUrl + `/expenses`, {
+      userId: userId,
+      description: details.description,
+      transactionDate: details.date,
+      totalBalance: parseFloat(details.amount),
+      notes: details.notes,
+      transactions: transactions,
+    });
+  } else {
+    alert("Please enter a valid amount.");
+  }
 }
