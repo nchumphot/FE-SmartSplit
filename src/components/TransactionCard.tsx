c2c034524d194f3df411cf0ba83d1f0eff0ab032
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-02-15 16:49:58 +0000

    Show owing in red and is owed in green

--- src/components/TransactionCard.tsx
+++ src/components/TransactionCard.tsx
@@ -9,19 +9,19 @@ export function TransactionCard(props: {
     return <h2>An unknown error has occured.</h2>;
   } else {
     return (
-      <div className="container border border-primary m-2 p-2">
+      <div className="container-lg border border-primary m-2 p-2">
         <h6>{dateTimeFormatter(props.transaction.transaction_date)}</h6>
         <Link to={`/expenses/${props.transaction.expense_id}`}>
           <h4>{props.transaction.description}</h4>
         </Link>
 
         {props.transaction.amount < 0 && (
-          <h4>
+          <h4 className="text-success">
             You lent <br />£{props.transaction.balance}
           </h4>
         )}
         {props.transaction.amount > 0 && (
-          <h4>
+          <h4 className="text-danger">
             You owe <br />£{props.transaction.balance}
           </h4>
         )}
