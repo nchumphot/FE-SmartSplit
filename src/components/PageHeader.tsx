3225ebcd96736b8a1199231a979635f9dd3514cd
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-02-15 16:11:48 +0000

    Edited the styling of PageHeader

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -6,22 +6,33 @@ export function PageHeader(props: {
   setUser: React.Dispatch<React.SetStateAction<IUser | undefined>>;
 }): JSX.Element {
   return (
-    <>
-      <h1>SmartSplit</h1>
-      {props.user !== undefined && (
-        <>
-          <p>{props.user.name}</p>
-          <Link to="/">
-            <button
-              onClick={() => {
-                props.setUser(undefined);
-              }}
-            >
-              Log out
-            </button>
-          </Link>
-        </>
-      )}
-    </>
+    <div className="bg bg-success p-2">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-9">
+            <Link to="/">
+              <h1 className="text-light">SmartSplit</h1>
+            </Link>
+          </div>
+          {props.user !== undefined && (
+            <>
+              <p className="col my-auto text-light">{props.user.name}</p>
+              <div className="col my-auto">
+                <Link to="/">
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      props.setUser(undefined);
+                    }}
+                  >
+                    Log out
+                  </button>
+                </Link>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
